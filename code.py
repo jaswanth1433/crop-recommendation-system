ad9cee85d1964d9e570e9630354af64263e2d2ed
jaswanth1433 <jaswanthnarala@gmail.com>
2026-04-03 00:08:49 +0530

    Added imports section comment

--- code.py
+++ code.py
@@ -1,1 +1,8 @@
 #Crop Recommendation System
+# Import libraries
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
