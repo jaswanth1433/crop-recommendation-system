f60be3b929b8e3e963afe82baddb24cad985cd94
jaswanth1433 <jaswanthnarala@gmail.com>
2026-04-03 00:15:15 +0530

    Added train test split section

--- code.py
+++ code.py
@@ -18,4 +18,30 @@ def load_data():
 
 # Feature Selection
 
+# Train Test Split
 
+@st.cache_resource
+def train_model(df):
+    X = df.drop("label", axis=1)
+    y = df["label"]
+
+    # Train-test split
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=42
+    )
+
+    # Tuned Random Forest Model
+    model = RandomForestClassifier(
+        n_estimators=50,
+        max_depth=5,
+        min_samples_split=5,
+        min_samples_leaf=2,
+        random_state=42
+    )
+
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+
+    return model, acc, X.columns
