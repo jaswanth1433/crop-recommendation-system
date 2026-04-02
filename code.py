bd98a9cb34708c88472ecfc1b1caf19d643a4d76
jaswanth1433 <jaswanthnarala@gmail.com>
2026-04-03 00:10:48 +0530

    Added dataset loading section

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 #Crop Recommendation System
+
 # Import libraries
 import streamlit as st
 import pandas as pd
@@ -6,3 +7,9 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
+
+# Load dataset
+@st.cache_data
+def load_data():
+    return pd.read_csv("C:/Users/JASWATH/Downloads/archive/Crop_recommendation.csv")
